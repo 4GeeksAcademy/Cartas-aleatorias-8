2b3ea431fb6141a7065b9fa6d2b49f2d5e97d128
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-06 17:56:00 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const symbols = ["♦", "♥", "♠", "♣"];
+    const numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+    const randomSymbolIndex = Math.floor(Math.random() * symbols.length); // este codigo genera un numero aleatorio entre 0 y 3
+    const randomNumberIndex = Math.floor(Math.random() * numbers.length); // este codigo genera un numero aleatorio entre 0 y 12
+
+    const resultFinal = numbers[randomNumberIndex]; // este codigo genera un numero aleatorio entre 0 y 12
+
+    document.getElementById("card").innerHTML = symbols[randomSymbolIndex]; // este codigo realiza la seleccion de la carta aleatoria
+    document.getElementById("theCard").className = ""; // este codigo limpia la clase de la carta
+    document.getElementById("theCard").classList.add(".card") // este codigo agrega la clase de la carta
+    document.getElementById("theCard").classList.add(getSymbolsClases(resultFinal)) // este codigo agrega la clase de la carta
+}
+
+    const getSymbolsClases = (resultFinal) => {
+      if (resultFinal === "Diamonds") {
+        return "suit-diamonds";
+      } else if (resultFinal === "Spades") {
+        return "suit-spades";
+      } else if (resultFinal === "Hearts") {
+        return "suit-hearts";
+      } else if (resultFinal === "Clubs") {
+        return "suit-clubs";
+      }
 };
