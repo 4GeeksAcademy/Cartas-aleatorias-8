5038c1e3230747fa12714c2c8b5db01b28fcfe74
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-03-06 19:11:04 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,28 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  const symbols = ["♦", "♥", "♠", "♣"];
+    const symbols = ["♦", "♥", "♠", "♣"];
     const numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
-    const randomSymbolIndex = Math.floor(Math.random() * symbols.length); // este codigo genera un numero aleatorio entre 0 y 3
-    const randomNumberIndex = Math.floor(Math.random() * numbers.length); // este codigo genera un numero aleatorio entre 0 y 12
+    const randomSymbolIndex = Math.floor(Math.random() * symbols.length); 
+    const randomNumberIndex = Math.floor(Math.random() * numbers.length); 
+     
+    const randomSymbol = symbols[randomSymbolIndex]
+    const randomNumber = numbers[randomNumberIndex]; 
 
-    const resultFinal = numbers[randomNumberIndex]; // este codigo genera un numero aleatorio entre 0 y 12
+    document.getElementById("cardContent").innerHTML = randomNumber;
 
-    document.getElementById("card").innerHTML = symbols[randomSymbolIndex]; // este codigo realiza la seleccion de la carta aleatoria
-    document.getElementById("theCard").className = ""; // este codigo limpia la clase de la carta
-    document.getElementById("theCard").classList.add(".card") // este codigo agrega la clase de la carta
-    document.getElementById("theCard").classList.add(getSymbolsClases(resultFinal)) // este codigo agrega la clase de la carta
+    const theCardE1 = document.getElementById("theCard")
+    theCardE1.className = "";
+    theCardE1.classList.add("card-body") 
+    theCardE1.classList.add(getSymbolsClases(randomSymbol))
+
 }
 
-    const getSymbolsClases = (resultFinal) => {
-      if (resultFinal === "Diamonds") {
+    // esta funcion
+    const getSymbolsClases = (symbol) => {
+      if (symbol === "♦") {
         return "suit-diamonds";
-      } else if (resultFinal === "Spades") {
+      } else if (symbol === "♠") {
         return "suit-spades";
-      } else if (resultFinal === "Hearts") {
+      } else if (symbol === "♥") {
         return "suit-hearts";
-      } else if (resultFinal === "Clubs") {
+      } else if (symbol === "♣") {
         return "suit-clubs";
       }
-};
+    };
